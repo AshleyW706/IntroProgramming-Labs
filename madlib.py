31ec4d8a2a7241bbdbb16dedde7c50068363e0c9
Ashley Wohlrab <Ashley.Wohlrab1@marist.edu>
2018-09-10 21:37:58 -0400

    Solution to mad libs exercise.

---
+++ madlib.py
@@ -0,0 +1,10 @@
+#Intro to Programming
+#Author: Ashley Wohlrab
+#Date: 10 September 2018
+
+noun = input('Enter a noun:')
+verb = input('Enter a verb:')
+adjective = input('Enter an adjective:')
+place = input('Enter a place:')
+
+print("The " + adjective + noun + "lives alone at " + place + " and " + verb)
