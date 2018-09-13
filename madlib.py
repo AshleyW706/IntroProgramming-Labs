9ab9198899d871cfa57f51e71606815701d75cf7
Ashley Wohlrab <Ashley.Wohlrab1@marist.edu>
2018-09-13 14:45:40 -0400

    Added three functions to program.

--- madlib.py
+++ madlib.py
@@ -1,10 +1,19 @@
 #Intro to Programming
 #Author: Ashley Wohlrab
 #Date: 10 September 2018
+    
+def promptForWords():
+    global noun, verb, adjective, place
+    noun = input("Enter a noun:")
+    verb = input("Enter a verb:")
+    adjective = input("Enter an adjective:")
+    place = input("Enter a place:")
 
-noun = input('Enter a noun:')
-verb = input('Enter a verb:')
-adjective = input('Enter an adjective:')
-place = input('Enter a place:')
+def makeAndPrintSentence():
+    print("The " + adjective + " " + noun + " lives alone at " + place + " and " + verb + ".")
 
-print("The " + adjective + noun + "lives alone at " + place + " and " + verb)
+def main():
+    promptForWords()
+    makeAndPrintSentence()
+
+main()
