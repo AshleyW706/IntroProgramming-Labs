3affeb2df3d9c9877ea0f6a82c517a11a56cdd84
Ashley Wohlrab <Ashley.Wohlrab1@marist.edu>
2018-10-18 18:44:22 -0400

    Usability Update, resolves issues #4 and #5.

--- guessing-game.py
+++ guessing-game.py
@@ -1,18 +1,35 @@
 #Ashley Wohlrab
 #Lab Activity #5
 
-
-def main():
-    print("PYTHON GUESSING GAME.")
-
-main()
-
-
 answer = "dolphin"
 
 def userInput():
         givenInput = input("Computer is thinking of an animal...").lower()
         return givenInput
+
+def likeAnimal():
+    Input = input("Do you like this animal?").lower()
+    return Input
+
+def startsWith():
+    x= 'quit'
+
+def askQuestion():
+    Game = True
+    while Game:
+        Input = likeAnimal()
+        if Input == "yes":
+            print("I do too! Cool!")
+            break
+        elif Input == "no":
+            print("I'm sorry to hear that!")
+            break
+        elif Input[0]=='q':
+            break
+        else:
+            print("Please respond with Yes or No")
+        
+
 
 def guessingGame():
     Game = True
@@ -20,8 +37,24 @@ def guessingGame():
         input = userInput()
         if input == "dolphin": 
             print("You have guessed the correct animal! Congratulations!")
+            askQuestion()
+            break
+        elif input[0]=='q':
             break
         else:
             print("That is not the correct animal. Please try again.")
+    
 
-guessingGame()
+
+def main():
+    print("PYTHON GUESSING GAME.")
+    guessingGame()
+    
+main()
+
+
+
+        
+        
+
+    
