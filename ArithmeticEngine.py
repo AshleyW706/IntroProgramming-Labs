b7d81fcd4e3da8d9270c515ef566bf33a8c3ef11
Ashley Wohlrab <Ashley.Wohlrab1@marist.edu>
2018-10-25 14:58:15 -0400

    Solving handling errors in Arithmetic function.

---
+++ ArithmeticEngine.py
@@ -0,0 +1,60 @@
+# CMPT 120 - Lab #6
+# Ashley Wohlrab
+# 25 October 2018
+###
+
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    print("\nPlease come back again soon!")
+
+def addFunc():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    return num1+num2
+
+def subFunct():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    return num1-num2
+
+def multFunc():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    return num1*num2
+
+def divFunc():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    return num1//num2
+    
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ").lower()
+        
+        if cmd == "add":
+            x = addFunc()
+        elif cmd == "sub":
+            x = subFunc()
+        elif cmd == "mult":
+            x = multFunc()
+        elif cmd == "div":
+            x = divFunc()
+        elif cmd == "quit":
+            break   
+        else:
+            print("That is not a valid command.")
+            continue
+    
+        print("The result is " + str(x) + ".\n")
+
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+
+main()
